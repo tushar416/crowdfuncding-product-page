51b7764e0a81d94806c8941666b8500576c2d13d
tushar416 <62717803+tushar416@users.noreply.github.com>
2021-04-11 09:14:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+let btnToggle = document.querySelector(".button-toggle");
+let nav = document.querySelector("nav");
+
+let show = false;
+btnToggle.addEventListener("click", () => {
+  if (!show) {
+    btnToggle.src = "images/icon-close-menu.svg";
+    nav.style.maxHeight = "400px";
+    show = true;
+  } else {
+    btnToggle.src = "images/icon-hamburger.svg";
+    nav.style.maxHeight = "0";
+    show = false;
+  }
+});
+
+let bookToggle = document.querySelector("#bookmark-toggle");
+let bookmarkTriggers = document.querySelectorAll("[data-toggle=bookmark]");
+
+bookmarkTriggers.forEach((bookmark) => {
+  bookmark.addEventListener("click", () => {
+    if (bookToggle.textContent == "Bookmark") {
+      bookToggle.textContent = "Bookmarked";
+    } else {
+      bookToggle.textContent = "Bookmark";
+    }
+  });
+});
+
+// modal support
+
+let supportTriggers = document.querySelectorAll("[data-toggle=modal-support]");
+let modalSupport = document.querySelector("#modal-support");
+
+supportTriggers.forEach((support) => {
+  support.addEventListener("click", () => {
+    modalSupport.classList.toggle("flex");
+  });
+});
+
+// main modal
+
+let modalTriggers = document.querySelectorAll(".modal-trigger");
+let modalMain = document.querySelector("#modal-container");
+
+modalTriggers.forEach((modal) => {
+  modal.addEventListener("click", () => {
+    modalMain.classList.toggle("show");
+  });
+});
+
+// border
+let borderChange = document.querySelectorAll(".modal-box");
+
+document.querySelectorAll("input[type=radio]").forEach((radio) => {
+  radio.addEventListener("click", () => {
+    borderChange.forEach((border) => {
+      if (border.querySelector("input[type=radio]").checked) {
+        border.style.borderColor = "hsl(176, 50%, 47%)";
+      } else {
+        border.style.borderColor = "hsl(0, 0%, 93%)";
+      }
+    });
+  });
+});
